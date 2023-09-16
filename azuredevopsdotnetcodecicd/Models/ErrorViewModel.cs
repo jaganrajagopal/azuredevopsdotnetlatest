a12bcf5e57434ad07604923283e34c132c72a377
Jagan Rajagopal <jaganrajagopal@outlook.com>
2023-09-16 15:15:05 +0530

    added the dotnet projects

---
+++ azuredevopsdotnetcodecicd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azuredevopsdotnetcodecicd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ dotnetunit/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace dotnetunit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
